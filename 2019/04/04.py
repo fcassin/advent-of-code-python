6b88d1a2f6c9aab570527053124768036c7210c2
Francois Cassin <francois.cassin@plakar.io>
2026-08-10 19:31:31 +0200

    feat: 2019, day 04

---
+++ 2019/04/04.py
@@ -0,0 +1,56 @@
+import sys
+
+sys.path.append("../../")
+
+import collections
+import functools
+import graph
+import grid
+import hashlib
+import ints
+import itertools
+import letter
+import math
+import screen
+
+INPUT = "382345-843167"
+
+
+def valid(value):
+    pairs = list(itertools.pairwise(str(value)))
+    return all(a <= b for a, b in pairs) and any(a == b for a, b in pairs)
+
+
+def valid_strict(value):
+    pairs = list(itertools.pairwise(str(value)))
+
+    if not all(a <= b for a, b in pairs):
+        return False
+
+    if not any(a == b for a, b in pairs):
+        return False
+
+    runs = [len(list(groups)) for _, groups in itertools.groupby(str(value))]
+    if 2 not in runs:
+        return False
+
+    return True
+
+
+def main():
+    print(part1(INPUT))
+    print(part2(INPUT))
+
+
+def part1(input):
+    lo, hi = [int(value) for value in input.split("-")]
+    return sum(map(valid, range(lo, hi + 1)))
+
+
+def part2(input):
+    lo, hi = [int(value) for value in input.split("-")]
+    return sum(map(valid_strict, range(lo, hi + 1)))
+
+
+if __name__ == "__main__":
+    main()
